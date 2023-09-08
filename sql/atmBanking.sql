691e847e6844572ed85ba0ee1e7fea3264ec01b6
Aryan Srivastava <86686659+aryansri07@users.noreply.github.com>
2023-09-08 11:49:50 +0530

    adding db sql code

---
+++ sql/atmBanking.sql
@@ -0,0 +1,32 @@
+/*create database atmBanking*/
+
+use atmBanking
+
+create table RegisteredUser
+(
+userid int primary key identity(500,1),
+uname varchar(20),
+dob date default getdate(),
+email varchar(20),
+proof varchar(12)
+)
+
+create table account(
+	aid int primary key identity(1000,1),
+	userid int references RegisteredUser(userid),
+	homeBranch varchar(50),
+	balance float,
+	accType varchar(3)
+)
+create table txn(
+	tid int primary key identity(100,1),
+	aid int references account(aid),
+	amount float,
+	txnTime date default getdate(),
+	loc varchar(6),
+	txnType varchar(3)
+)
+
+insert into RegisteredUser(uname, email, proof) values('Ram', 'ram@abc.com', '123456');
+insert into account(homeBranch, balance, accType) values('Hyderabad', 10000, 'SAV');
+insert into txn(amount, loc, txnType) values(200, 'ABCXYZ', 'DEB')
