7cb1e9f488722554a2c266852f5ed6a01d455e89
unknown <ishaanagarwal1805@gmail.com>
2023-09-12 00:11:03 +0530

    Constructing sign in page

--- frontend/src/Pages/SignIn.js
+++ frontend/src/Pages/SignIn.js
@@ -3,6 +3,36 @@ import ReactDOM from 'react-dom/client';
 import { useState } from 'react';
 
 const SignIn = () =>{
+    const [username, setusername] = useState(' ');
+    const [password, setpassword] = useState(' ');    
 
+    const handleUsername = (event) =>{
+        setusername(event.target.value);
+
+    }
+
+    const handlepassword = (event) =>{
+        setpassword(event.target.value);
+
+    }
+
+    return (
+        <div>
+            <form >
+                <br/><br/>
+                <div>
+                    username: <input type = 'text' value = {username} onChange={handleUsername} /> 
+                </div>
+                <br/>
+                <div>
+                    password: <input type = 'alphanum' value = {password} onChange={handlepassword} ></input> 
+                </div>
+                <br/>
+                <div>
+                    <button type = 'submit'>Sign In</button>
+                </div>
+            </form>
+        </div>
+    )
 }
 export default SignIn;
