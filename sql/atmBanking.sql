d30c12c5a53a3755f0d80e2019834fc9b29496a9
Rwitick <144416013+rwitick97@users.noreply.github.com>
2023-09-12 10:35:47 +0530

    feat: db update

--- sql/atmBanking.sql
+++ sql/atmBanking.sql
@@ -1,15 +1,17 @@
-/*create database atmBanking*/
+/*create database atmBanking;*/
 
-use atmBanking
+use atmBanking;
 
 create table RegisteredUser
 (
-userid int primary key identity(500,1),
+userid int primary key identity(5000,1),
 uname varchar(20),
 dob date default getdate(),
 email varchar(20),
-proof varchar(12)
-)
+proof varchar(12),
+isAdmin bit,
+pass varchar(128)
+);
 
 create table account(
 	aid int primary key identity(1000,1),
@@ -17,7 +19,8 @@ create table account(
 	homeBranch varchar(50),
 	balance float,
 	accType varchar(3)
-)
+);
+
 create table txn(
 	tid int primary key identity(100,1),
 	aid int references account(aid),
@@ -25,8 +28,9 @@ create table txn(
 	txnTime date default getdate(),
 	loc varchar(6),
 	txnType varchar(3)
-)
+);
 
-insert into RegisteredUser(uname, email, proof) values('Ram', 'ram@abc.com', '123456');
+
+/*insert into RegisteredUser(uname, email, proof) values('Ram', 'ram@abc.com', '123456');
 insert into account(homeBranch, balance, accType) values('Hyderabad', 10000, 'SAV');
-insert into txn(amount, loc, txnType) values(200, 'ABCXYZ', 'DEB')
+insert into txn(amount, loc, txnType) values(200, 'ABCXYZ', 'DEB')*/
