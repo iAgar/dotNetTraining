218c2097c7ef2b67ea7b439cce9f6638a281c4a9
Rwitick <144416013+rwitick97@users.noreply.github.com>
2023-09-17 22:31:22 +0530

    chore: move utils to a new file

--- backend/Services/IAuthService.cs
+++ backend/Services/IAuthService.cs
@@ -5,11 +5,7 @@ namespace backend.Services
 {
     public interface IAuthService
     {
-        string HashPasswordV2(string password, RandomNumberGenerator rng);
-        bool VerifyHashedPasswordV2(string hashedPassword, string password);
         Tuple<RegisteredUser?, string> Login(UserDto user);
         int Register(RegisteredUser user);
-        string? CreateToken(RegisteredUser user);
-        int? ValidateToken(string? token);
     }
 }
