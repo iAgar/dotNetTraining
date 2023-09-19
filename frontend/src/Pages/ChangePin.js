faecd0ba9a85b51539e0bd6923ad1fa3e06e0408
Ishaan <ishaanagarwal1805@gmail.com>
2023-09-19 14:34:18 +0530

    Implement change pin functionality

--- frontend/src/Pages/ChangePin.js
+++ frontend/src/Pages/ChangePin.js
@@ -1,20 +1,22 @@
 import React, { useContext, useState } from "react";
+import { UserContext } from "./userContext.js";
 import axios from 'axios';
 import './SignIn.css';
 import './SignUp.css';
 
 const ChangePin =() =>{
     
-    const [userDetails, setPinChange] = useState({
-        'aid': 0,
-        'oldPin': '',
-        'newPin' :'',
+    const [pinChangeDetails, setPinChange] = useState({
+        'Aid': '',
+        'Pin': '',
+        'NewPin' :'',
     });
+    const userDetails = useContext(UserContext);
     // const user = useContext(UserContext);
     const[error,setError] = useState();
 
     const handleChange =(event) =>{
-        setPinChange({...userDetails, [event.target.name] : event.target.value})
+        setPinChange({...pinChangeDetails, [event.target.name] : event.target.value})
     }
 
     const handleSubmit = async(event) => {
@@ -23,9 +25,9 @@ const ChangePin =() =>{
             'Content-type': 'application/json',
             'Authorization': `bearer ${userDetails.token}`
         }
-
+        console.log(pinChangeDetails);
         try {
-            axios.post(`https://localhost:7180/changepin/${userDetails.aid}`, userDetails,{
+            axios.post(`https://localhost:7180/changepin/${pinChangeDetails.Aid}`, pinChangeDetails,{
                 headers: headers
             }).then((response) => {
                 console.log(response);
@@ -45,16 +47,16 @@ const ChangePin =() =>{
             <h1>Change PIN</h1>
             <form onSubmit={handleSubmit} class = "form-group form-label ">
                 <div>
-                    Account ID <br/> <input class = 'form-input' name='aid' type='number' />
+                    Account ID <br/> <input class = "form-input" name='Aid' type='number' value={userDetails.Aid} onChange={handleChange}/>
                 </div>
                 <br/>
 
                 <div>
-                    Old PIN: <br/><input class = "form-input" name='oldPin' type = 'text' maxLength={4} minLength={4} value = {userDetails.oldPin} onChange={handleChange}/> 
+                    Old PIN: <br/><input class = "form-input" name='Pin' type = 'text' maxLength={4} minLength={4} value = {userDetails.Pin} onChange={handleChange}/> 
                 </div>
                 <br/>
                 <div>
-                    New PIN: <br/><input class = "form-input" name = 'newPin' type = 'text' maxLength={4} minLength={4}  value = {userDetails.newPin} onChange={handleChange} /> 
+                    New PIN: <br/><input class = "form-input" name = 'NewPin' type = 'text' maxLength={4} minLength={4}  value = {userDetails.NewPin} onChange={handleChange} /> 
                 </div>
                 <br/> 
                
