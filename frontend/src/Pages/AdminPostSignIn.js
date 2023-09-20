eefa264010beea03cc973a9d7e9666e7b68d03fa
Ishaan <ishaanagarwal1805@gmail.com>
2023-09-20 15:30:22 +0530

    view users list on admin dashboard

--- frontend/src/Pages/AdminPostSignIn.js
+++ frontend/src/Pages/AdminPostSignIn.js
@@ -1,17 +1,52 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import { useState } from 'react';
 import { UserContext } from './userContext.js';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const AdminPostSignIn = () =>{
+
+const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    setLoading(true);
+    axios.get("https://localhost:7180/api/Users/all").then((response) => {
+        console.log(response);
+        setUsers(response.data.users);
+        setLoading(false)
+      })
+  }, [])
+
     return (
         <div>
             <Link  to='/SignUp'>Sign Up new User</Link>
-            <br/><br/> <br/><br/>
             <Link to='/CreateAccount'>Create new Bank Account</Link>
+            {loading ? (<div>Loading ...</div>):(
+                <div>
+                <h1>Users</h1>
+                <table border={1}>
+                  <tr>
+                    <th>User Id</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                  </tr>
+                  {users.map(user => {return (
+                    <tr key={user.userid}>
+                      <td><Link to={
+                        {
+                            pathname:'/accounts',
+                            userId: `${user.userid}`
+                        }
+                      }>{user.userid}</Link></td>
+                      <td>{user.uname}</td>
+                      <td>{user.email}</td>
+                    </tr>
+                  )})}
+                </table>
+                </div>
+            )}
         </div>
-            
+        
     )
 }
 
