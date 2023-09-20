4a38b0eecf7b6a0916108ef1baf7fc0d1b85f090
Rwitick <144416013+rwitick97@users.noreply.github.com>
2023-09-20 09:57:47 +0530

    fix: location of test folder

--- backend.Tests/BasicUnitTest.cs
+++ backend.Tests/BasicUnitTest.cs
@@ -4,7 +4,7 @@ namespace backend.Tests
     public class BasicUnitTest
     {
         [TestMethod]
-        public void TestMethod()
+        public void TestMethod1()
         {
             Assert.AreEqual(1, 1);
             Assert.AreNotEqual(1, 0);
