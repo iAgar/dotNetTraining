f522d8ec3cf03889738671d7cb28af17427db8b7
Ishaan <ishaanagarwal1805@gmail.com>
2023-09-22 15:42:32 +0530

    change req headers

--- frontend/src/Pages/Accounts.js
+++ frontend/src/Pages/Accounts.js
@@ -2,15 +2,21 @@ import React, { useContext, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { UserContext } from './userContext.js';
 
 const Accounts = (props) => {
     const {userid} = useParams()
     console.log(userid);
     const [accounts, setAccounts] = useState([])
+    const userDetails = useContext(UserContext);
     const [loading, setLoading] = useState(false)
     useEffect(() => {
     setLoading(true);
-    axios.get(`https://localhost:7180/api/Accounts/all/${userid}`).then((response) => {
+    const headers = {
+        'Content-type': 'application/json',
+        'Authorization': `bearer ${userDetails.token}`
+    }
+    axios.get(`https://localhost:7180/api/Accounts/all/${userid}`,{headers: headers}).then((response) => {
         console.log(response);
         setAccounts(response.data.accounts);
         setLoading(false)
