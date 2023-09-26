e6857dc58ef1e07023a7ebb286c66413e0048e23
Pallavi <pallaviub12@gmail.com>
2023-09-26 18:35:46 +0530

    feat: improved landing page

--- frontend/src/Pages/WelcomePage.js
+++ frontend/src/Pages/WelcomePage.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import './WelcomePage.css'; 
+import './WelcomePage.css';
+import img1 from './Images/img1.PNG' 
+import img2 from './Images/img2.PNG' 
+import img3 from './Images/img3.PNG' 
 
 function WelcomePage() {
   return (
@@ -9,6 +12,17 @@ function WelcomePage() {
         <p>Your Trusted Banking Partner</p>
       </header>
       <main>
+        <section class="image-gallery">
+            <div class="gallery-item">
+                <img src={img1} alt="Image 1"></img>
+            </div>
+            <div class="gallery-item">
+                <img src={img2} alt="Image 2"/>
+            </div>
+            <div class="gallery-item">
+                <img src={img3} alt="Image 3"></img>
+            </div>
+        </section>
         <p>Secure, Simple, and Convenient Banking Services</p>
       </main>
       <footer>
